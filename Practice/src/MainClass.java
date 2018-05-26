0e044a75329aab5b5a75b307fcf86528277a254d
이예린\Yerin <ye_reen@naver.com>
2018-05-27 00:50:38 +0900

    API1

---
+++ Practice/src/MainClass.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;;
+public class MainClass {
+	
+	public static void main(String args[]) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		String str2 = scanner.nextLine();
+		System.out.println(str2);;
+		
+		String str1 = scanner.next();
+		
+		
+	}
+	
+
+}
